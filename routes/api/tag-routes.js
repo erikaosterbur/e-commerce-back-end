5468b427d0754b53ae903325fde9e9d027ca520a
erikaosterbur <erikaosterbur@gmail.com>
2021-10-27 19:36:58 -0500

    routes finished

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,14 +37,43 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+    Tag.update( req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(updatedTag => res.json(updatedTag))
+    .catch(err => res.json(err))
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with that id"});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
